1f3e1911eb8622509f71f4d26a2c9902996eba0a
mlquadri <mlquadri@stu.naperville203.org>
2017-01-11 15:08:40 -0600

    Worked on partner lab

--- PictureLab/classes/IntArrayWorker.java
+++ PictureLab/classes/IntArrayWorker.java
@@ -1,117 +1,154 @@
 public class IntArrayWorker
 {
-  /** two dimensional matrix */
-  private int[][] matrix = null;
-  
-  /** set the matrix to the passed one
-    * @param theMatrix the one to use
-    */
-  public void setMatrix(int[][] theMatrix)
-  {
-    matrix = theMatrix;
-  }
-  
-  /**
-   * Method to return the total 
-   * @return the total of the values in the array
-   */
-  public int getTotal()
-  {
-    int total = 0;
-    for (int row = 0; row < matrix.length; row++)
+    /** two dimensional matrix */
+    private int[][] matrix = null;
+
+    /** set the matrix to the passed one
+     * @param theMatrix the one to use
+     */
+    public void setMatrix(int[][] theMatrix)
     {
-      for (int col = 0; col < matrix[0].length; col++)
-      {
-        total = total + matrix[row][col];
-      }
+        matrix = theMatrix;
     }
-    return total;
-  }
-  
-  /**
-   * Method to return the total using a nested for-each loop
-   * @return the total of the values in the array
-   */
-  public int getTotalNested()
-  {
-    int total = 0;
-    for (int[] rowArray : matrix)
+
+    /**
+     * Method to return the total 
+     * @return the total of the values in the array
+     */
+    public int getTotal()
     {
-      for (int item : rowArray)
-      {
-        total = total + item;
-      }
+        int total = 0;
+        for (int row = 0; row < matrix.length; row++)
+        {
+            for (int col = 0; col < matrix[0].length; col++)
+            {
+                total = total + matrix[row][col];
+            }
+        }
+        return total;
     }
-    return total;
-  }
-  
-  /**
-   * Method to fill with an increasing count
-   */
-  public void fillCount()
-  {
-    int numCols = matrix[0].length;
-    int count = 1;
-    for (int row = 0; row < matrix.length; row++)
+
+    /**
+     * Method to return the total using a nested for-each loop
+     * @return the total of the values in the array
+     */
+    public int getTotalNested()
     {
-      for (int col = 0; col < numCols; col++)
-      {
-        matrix[row][col] = count;
-        count++;
-      }
+        int total = 0;
+        for (int[] rowArray : matrix)
+        {
+            for (int item : rowArray)
+            {
+                total = total + item;
+            }
+        }
+        return total;
     }
-  }
-  
-  /**
-   * print the values in the array in rows and columns
-   */
-  public void print()
-  {
-    for (int row = 0; row < matrix.length; row++)
+
+    /**
+     * Method to fill with an increasing count
+     */
+    public void fillCount()
     {
-      for (int col = 0; col < matrix[0].length; col++)
-      {
-        System.out.print( matrix[row][col] + " " );
-      }
-      System.out.println();
+        int numCols = matrix[0].length;
+        int count = 1;
+        for (int row = 0; row < matrix.length; row++)
+        {
+            for (int col = 0; col < numCols; col++)
+            {
+                matrix[row][col] = count;
+                count++;
+            }
+        }
     }
-    System.out.println();
-  }
-  
-  public int getCount(int num)
-  {
-      int count=0;
-      for(int[] aValue: matrix)
-      {
-          for(int fValue: aValue)
-          {
-             if(fValue==num)
-             {
-              count++;
-             }
-          }
-      }
-      return count;
-  }
+
+    /**
+     * print the values in the array in rows and columns
+     */
+    public void print()
+    {
+        for (int row = 0; row < matrix.length; row++)
+        {
+            for (int col = 0; col < matrix[0].length; col++)
+            {
+                System.out.print( matrix[row][col] + " " );
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public int getLargest()
+    {
+        int largest = matrix[0][0];
+        for(int[] aValue: matrix)
+        {
+            for(int fValue: aValue)
+            {
+                if(fValue>largest)
+                {
+                    largest=fValue;
+                }
+            }
+        }
+        return largest;
+    }
+
+    public int getCount(int num)
+    {
+        int count=0;
+        for(int[] aValue: matrix)
+        {
+            for(int fValue: aValue)
+            {
+                if(fValue==num)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int getColTotal(int colnum)
+    {
+        int count = 0;
+        for (int[] row: matrix)
+        {
+            count +=row[colnum];
+        }
+        return count;
+    }
 
     /** 
-   * fill the array with a pattern
-   */
-  public void fillPattern1()
-  {
-    for (int row = 0; row < matrix.length; row++)
+     * fill the array with a pattern
+     */
+    public void fillPattern1()
     {
-      for (int col = 0; col < matrix[0].length; 
-           col++)
-      {
-        if (row < col)
-          matrix[row][col] = 1;
-        else if (row == col)
-          matrix[row][col] = 2;
-        else
-          matrix[row][col] = 3;
-      }
+        for (int row = 0; row < matrix.length; row++)
+        {
+            for (int col = 0; col < matrix[0].length; 
+            col++)
+            {
+                if (row < col)
+                    matrix[row][col] = 1;
+                else if (row == col)
+                    matrix[row][col] = 2;
+                else
+                    matrix[row][col] = 3;
+            }
+        }
     }
-  }
- 
+
 }
